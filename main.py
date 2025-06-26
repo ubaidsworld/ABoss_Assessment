9c4ed16e552652fa2c46ee92c38566845373e717
Ubaidullah Khan <33365560+ubaidsworld@users.noreply.github.com>
2025-06-26 20:42:15 +0530

    Newly added files

---
+++ main.py
@@ -0,0 +1,25 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+import joblib
+
+app = FastAPI()
+
+model = joblib.load("spam_model.pkl")
+vectorizer = joblib.load("vectorizer.pkl")
+
+class TextIn(BaseModel):
+    text: str
+
+@app.post("/predict")
+def predict(data: TextIn):
+    x = vectorizer.transform([data.text])
+    prediction = model.predict(x)[0]
+    label = "Spam" if prediction == 1 else "Not Spam"
+    return {"prediction": label}
+
+@app.get("/", response_class=HTMLResponse)
+def homepage():
+    with open("templates//index.html", "r") as f:
+        return f.read()
---
+++ train.py
@@ -0,0 +1,16 @@
+# train_model.py
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+texts = ["Free offer now!!!", "Hi Bob, are we still meeting today?", "Buy cheap meds", "Let's catch up soon"]
+labels = [1, 0, 1, 0]  # 1: Spam, 0: Not Spam
+
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(texts)
+
+model = MultinomialNB()
+model.fit(X, labels)
+
+joblib.dump(model, "spam_model.pkl")
+joblib.dump(vectorizer, "vectorizer.pkl")
